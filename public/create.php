16882cf831057446ad693ed7fc9b6ba29fd6306b
puspa6 <alinnovtech@gmail.com>
2025-12-11 17:02:56 +0100

    creation des partials

---
+++ public/create.php
@@ -0,0 +1,24 @@
+    <?php 
+       $title = "Nouveau film";
+       $description = "Ajout d'un nouveau film";
+       $keywords = "Cinéma, repertoire, ajout, nouveau, film, dwwm22";
+    ?>
+    
+    
+    
+    <?php include_once __DIR__ . "/../partials/head.php"; ?>
+
+    <?php include_once __DIR__ . "/../partials/nav.php"; ?>
+
+
+
+    <!-- Main -->
+    <main class="container">
+        <h1 class="text-center my-3 display-5">Nouveau film</h1>
+
+
+    </main>
+
+    <?php include_once __DIR__ . "/../partials/footer.php"; ?>
+
+    <?php include_once __DIR__ . "/../partials/foot.php"; ?>
