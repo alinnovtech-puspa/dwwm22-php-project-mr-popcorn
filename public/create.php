8f16d9605e02e36c54f84f8ec5e71ec5e74c5755
puspa6 <alinnovtech@gmail.com>
2025-12-16 17:01:20 +0100

    recupération des films et affichage de la liste

--- public/create.php
+++ public/create.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+require_once __DIR__ . "/../functions/db.php";
+
 // var_dump($_SERVER); 
 // die();
 
@@ -74,6 +76,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         }
     }
 
+
     // 4. S'il existe au moins une erreur détectée par le système,
     if (count($formErrors) > 0) {
         // Alors,
@@ -81,6 +84,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         $_SESSION['form_errors'] = $formErrors;
 
         // 4b. Sauvegarder les anciennes données provenant du formulaire en session
+        $_SESSION['old'] = $_POST;
 
         // 4c. Effectuer une redirection vers la page de laquelle proviennent les informations
         // Puis arrêter l'exécution du script.
@@ -88,20 +92,28 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         die();
     }
 
-    die('Continuer la partie');
+    // die('Continuer la partie');
 
 
     // 5. Dans le cas contraire,
     // 5a. Arrondir la note à un chiffre après la virgule,
+    $ratingRounded = null;
+
+if ( isset($_POST['rating']) && $_POST['rating'] !=="" ) {
+    $ratingRounded = round($_POST['rating'], 1);
+}
 
     // 6. Etablir une connexion avec la base de données
-
     // 7. Effectuer la requête d'insertion du nouveau film dans la table prévue (film)
+    insertFilm($ratingRounded, $_POST);
 
     // 8. Générer le message flash de succès
+    $_SESSION['success'] = "Le film a été ajouter à la liste avec succès.";
 
     // 9. Effectuer une redirection vers la page listant les films ajoutés (index.php)
     // Puis arrêter l'exécution du script.
+    header("Location: index.php");
+    die();
 }
 
 // Générons et sauvegardons le jéton de sécurité en session
@@ -126,7 +138,7 @@ $keywords = "Cinema, répertoire, ajout, nouveau, film, dwwm22";
             <div class="col-md-8 col-lg-4 mx-auto p-4 bg-white shadow rounded">
 
                 <?php if (isset($_SESSION['form_errors']) && !empty($_SESSION['form_errors'])) : ?>
-                    <div class="alert alert-danger">
+                    <div class="alert alert-danger" role="alert">
                         <ul>
                             <?php foreach ($_SESSION['form_errors'] as $error) : ?>
                                 <li><?= $error; ?></li>
@@ -139,15 +151,18 @@ $keywords = "Cinema, répertoire, ajout, nouveau, film, dwwm22";
                 <form method="post">
                     <div class="mb-3">
                         <label for="title">Titre <span class="text-danger">*</span></label>
-                        <input type="text" name="title" id="title" class="form-control" autofocus required>
+                        <input type="text" name="title" id="title" class="form-control" autofocus required value="<?= isset($_SESSION['old']['title']) && !empty($_SESSION['old']['title']) ? htmlspecialchars($_SESSION['old']['title']) : '';unset($_SESSION['old']['title']); ?>">
                     </div>
                     <div class="mb-3">
                         <label for="rating">Note / 5</label>
-                        <input type="number" min="0" max="5" step="0.5" inputmode="decimal" name="rating" id="rating" class="form-control">
+                        <input type="number" min="0" max="5" step="0.5" inputmode="decimal" name="rating" id="rating" class="form-control" value="<?= isset($_SESSION['old']['rating']) && $_SESSION['old']['rating'] != "" ? htmlspecialchars($_SESSION['old']['rating']) : '';unset($_SESSION['old']['rating']); ?>">
                     </div>
                     <div class="mb-3">
                         <label for="comment">Laissez un commentaire</label>
-                        <textarea name="comment" id="comment" class="form-control" rows="4"></textarea>
+                        <textarea name="comment" id="comment" class="form-control" rows="4"><?= isset($_SESSION['old']['comment']) && !empty($_SESSION['old']['comment']) ? htmlspecialchars($_SESSION['old']['comment']) : '';unset($_SESSION['old']['comment']); ?></textarea>
+                        <small id="comment-counter">
+                            0 / 1000 caractères
+                        </small>
                     </div>
                     <input type="hidden" name="csrf_token" value="<?= $_SESSION['csrf_token']; ?>">
                     <input type="hidden" name="honey_pot" value="">
